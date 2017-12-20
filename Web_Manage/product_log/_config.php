ac78303c4317cb02847503ab95c7c7da17faa9ed
naughtyjoint <wateleee@gmail.com>
2017-12-20 13:53:01 +0800

    add product_folder and add new column in product

---
+++ Web_Manage/product_log/_config.php
@@ -0,0 +1,20 @@
+<?php
+$inc_path="../../inc/";
+$manage_path="../";
+$main_auth_key='product';
+$fun_auth_key='pro_log';
+include('../_config.php');
+
+$auth=coderAdmin::Auth($fun_auth_key);
+
+$table=coderDBConf::$product_log;
+$colname=coderDBConf::$col_product_log;
+
+
+$orderColumn=$colname["id"];
+$orderDesc='desc';
+$page_title=$auth['name'];
+$page=request_pag("page");
+$page_desc="{$page_title}-您可將內容修改為希望呈現的內容。";
+$mtitle='<li class="active">'.$page_title.'</li>';
+$mainicon=$auth['icon'];
---
+++ Web_Manage/product_log/service.php
@@ -0,0 +1,44 @@
+<?php
+include_once('_config.php');
+include_once('filterconfig.php');
+$errorhandle=new coderErrorHandle();
+try{
+	coderAdmin::vaild($auth,'view');
+	
+	$db = Database::DB();
+	$sHelp=new coderSelectHelp($db);
+	$sHelp->select="t.*,u.`{$colname_u['id']}` as uid,u.`{$colname_u['name']}`
+					,g.`{$colname_g['id']}` as platform,g.`{$colname_g['name']}`";
+	$sHelp->table=$table." t
+			LEFT JOIN $table_u u ON u.`{$colname_u['id']}` = t.`{$colname['user_id']}`
+			LEFT JOIN $table_g g ON g.`{$colname_g['id']}` = t.`{$colname['game_id']}`";
+	$sHelp->page_size=get("pagenum");
+	$sHelp->page=get("page");
+	$sHelp->orderby="updated_time";
+	//$sHelp->orderdesc=get("orderdesc",1);
+
+	$sqlstr=$filterhelp->getSQLStr();
+	$where = $sqlstr->SQL;
+	$sHelp->where=$where;
+
+	$rows=$sHelp->getList();
+	for($i=0;$i<count($rows);$i++){
+		$rows[$i][$colname['is_pay']]=coderHelp::getAryVal($langary_transfers,$rows[$i][$colname['is_pay']]);
+	}
+
+	$result['result']=true;
+	$result['data']=$rows;
+	$result['page']=$sHelp->page_info;
+	echo json_encode($result);
+}
+catch(Exception $e){
+	$errorhandle->setException($e); // 收集例外
+}
+
+if ($errorhandle->isException()) {
+	$result['result']=false;
+    $result['data']=$errorhandle->getErrorMessage();
+	echo json_encode($result);
+}
+
+?>
