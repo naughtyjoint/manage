a0d0ed1b46ee5e74bb61fce5a330666000d86ed8
naughtyjoint <wateleee@gmail.com>
2018-01-15 11:26:33 +0800

    修正Mycard交易比對錯誤

--- api/MyCardCompare.php
+++ api/MyCardCompare.php
@@ -16,11 +16,15 @@ if(isset($_POST["StartDateTime"]) && !empty($_POST["StartDateTime"]) && isset($_
 }else if(isset($_POST["MyCardTradeNo"]) && !empty($_POST["MyCardTradeNo"])){
     $qry = "SELECT PaymentType,TradeSeq,MyCardTradeNo,FacTradeSeq,member_id,Amount,Currency,Pay_time FROM mycard WHERE MyCardTradeNo = :MyCardTradeNo";
     $result = $db->query_prepare_first($qry,[":MyCardTradeNo" => post('MyCardTradeNo',1)]);
-    extract($result);
-    $date = new DateTime($Pay_time);
-    $date_str = $date->format('Y-m-d')."T".$date->format('H:i:s');
-    $result_String = $PaymentType.",".$TradeSeq.",".$MyCardTradeNo.",".$FacTradeSeq.",".$member_id.",".$Amount.",".$Currency.",".$date_str."<BR>";
-    echo $result_String;
+    if($result==false){
+        echo "No Data";
+    }else{
+        extract($result);
+        $date = new DateTime($Pay_time);
+        $date_str = $date->format('Y-m-d')."T".$date->format('H:i:s');
+        $result_String = $PaymentType.",".$TradeSeq.",".$MyCardTradeNo.",".$FacTradeSeq.",".$member_id.",".$Amount.",".$Currency.",".$date_str."<BR>";
+        echo $result_String;
+    }
 }else{
     echo $return_fal;
 }
