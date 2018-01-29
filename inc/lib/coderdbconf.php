063b6b4f2386289924aa1a30dba7085620b1f2b6
naughtyjoint <wateleee@gmail.com>
2018-01-29 10:48:52 +0800

    更新member資料表欄位

--- inc/lib/coderdbconf.php
+++ inc/lib/coderdbconf.php
@@ -28,7 +28,7 @@ class coderDBConf
     public static $anchor = 'anchor'; //主播
     public static $col_anchor = array('id'=>'id','name'=>'name','email'=>'email','point'=>'point','create_time'=>'created_time','update_time'=>'update_time','manager'=>'last_manager');
     public static $member = 'member'; //會員
-    public static $col_member = array('id'=>'id','member_id'=>'member_id','name'=>'member_name','platform_id'=>'platform_id','agent_id'=>'agent_id','email'=>'email','point'=>'point','create_time'=>'created_time','update_time'=>'updated_time','manager'=>'last_manager');
+    public static $col_member = array('id'=>'id','member_id'=>'member_id','name'=>'member_name','platform_id'=>'platform_id','agent_id'=>'agent_id','email'=>'email','point'=>'point','create_time'=>'created_at','update_time'=>'updated_at','manager'=>'last_manager');
     public static $agent = 'agent'; //代理
     public static $col_agent = array('id'=>'id','agent_id'=>'agent_id','name'=>'agent_name','email'=>'email','created_time'=>'created_time','updated_time'=>'updated_time','manager'=>'last_manager');
     public static $deposit = 'deposit'; //入款申請
